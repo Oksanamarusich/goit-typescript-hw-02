29fe3517b2e07c682d32b1871841276f824f0bef
Oksanamarusich <oksana.m229@gmail.com>
2023-11-22 21:46:48 +0200

    add

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,13 +2,21 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
-class Component {
+interface ComponentProps {
+  title: string;
+}
+
+class Component<T extends ComponentProps>{
   constructor (public props:T) {
 
   }
 }
 
-class Page extends Component {
+class PageProps implements ComponentProps{
+  title: string;
+}
+
+class Page extends Component<PageProps> {
   pageInfo () {
     console.log(this.props.title);
   }
